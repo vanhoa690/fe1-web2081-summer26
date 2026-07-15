00193fa17467d1e291f9c51dbc80d7b642ebe803
vanhoa690 <vanhoa690@gmail.com>
2026-07-15 19:27:51 +0700

    confirm xoa

--- src/app/pages/stories/stories.ts
+++ src/app/pages/stories/stories.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 interface Story {
+  id: number;
   title: string;
   author: string;
   views: number;
@@ -20,10 +21,27 @@ export class Stories {
   constructor(private http: HttpClient) {}
   // genric type <T>
   ngOnInit() {
-    this.http.get<Story[]>(' http://localhost:3000/stories').subscribe({
+    this.http.get<Story[]>('http://localhost:3000/stories').subscribe({
       next: (data) => {
         console.log(data);
         this.stories = data;
+      },
+      error: () => {
+        console.log('error');
+      },
+    });
+  }
+
+  // method delete
+  deleteStory(id: number) {
+    console.log(id);
+    // confirm truoc khi xoa
+    if (!confirm('Xoa ko')) return;
+
+    this.http.delete(`http://localhost:3000/stories/${id}`).subscribe({
+      next: () => {
+        // remove item co id can xoa: filter trong js
+        this.stories = this.stories.filter((story) => story.id !== id);
       },
       error: () => {
         console.log('error');
