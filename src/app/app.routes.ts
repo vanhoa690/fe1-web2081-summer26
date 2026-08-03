d83f6bcabfe72aac3ae42d77087f7a65ad66f130
vanhoa690 <vanhoa690@gmail.com>
2026-08-03 18:57:03 +0700

    auth-guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { Stories } from './pages/stories/stories';
 import { AddStory } from './add-story/add-story';
 import { EditStory } from './edit-story/edit-story';
 import { Register } from './pages/register/register';
+import { authGuard } from './guard/auth-guard';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   {
@@ -30,13 +32,19 @@ export const routes: Routes = [
   {
     path: 'add-story',
     component: AddStory,
+    canActivate: [authGuard],
   },
   {
     path: 'edit/:id',
     component: EditStory,
+    canActivate: [authGuard],
   },
   {
     path: 'register',
     component: Register,
+  },
+  {
+    path: 'login',
+    component: Login,
   },
 ];
---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  handleLogin() {
+    localStorage.setItem('token', 'token_xxx');
+  }
+}
